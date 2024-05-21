e4d86836975c557ea3d41cefa78e05d4449f1215
David Hankin <d_hankin@outlook.com>
2024-05-21 11:00:20 +0200

    fetch user admin/employee

---
+++ src/user/User.tsx
@@ -0,0 +1,61 @@
+import {useEffect, useState } from 'react'
+import Admin from './admin/Admin'
+import Employee from './employee/Employee'
+
+interface User {
+  email: string,
+  name: string,
+  authorized: number,
+  role: string
+}
+
+function User() {
+
+    const [userAuthority, setUserAuthority] = useState<string>("");
+    const [jwtToken, setJwtToken] = useState<string>("");
+    const [username, setUsername] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+
+    useEffect (() => {
+        let jsonwebtoken: string | null = "";
+        if(localStorage.getItem("jsonwebtoken") && jsonwebtoken !== null) {
+            jsonwebtoken = localStorage.getItem("jsonwebtoken");
+            setJwtToken(jsonwebtoken!);
+        }
+    }, []);
+
+    useEffect (() => {
+      getUserInformation();
+    }, [jwtToken]);
+
+    const getUserInformation = () => {
+      const fetchHTTP: string = "https://goldfish-app-jlmay.ondigitalocean.app/user/get-user";
+      fetch(fetchHTTP, {
+        method: "GET",
+        headers: {
+          "Authorization": jwtToken
+        }
+      }).then(res => {
+        if(!res.ok) {
+          new Error("No user found!");
+        }
+        return res.json();
+      }).then(data => {
+        console.log(data);
+        setUserAuthority(data.role);
+        setUsername(data.name);
+        setEmail(data.email);
+      }).catch((error) => {
+        console.log(error)
+      });
+    }
+
+  return (
+    <div>
+        <p>Hej {username + ", " + email}</p>
+        { userAuthority === "66446a0b97b346b20fd35b73" ? <Admin /> : <Employee /> }
+    </div>
+  )
+}
+
+export default User
